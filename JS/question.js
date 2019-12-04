b3742cdf79d420b507c0aea2c62ed66f71f49cc5
Suzy Nakayama <suzynakayama@Suzys-MacBook-Pro.local>
2019-12-03 19:43:29 -0500

    nothing

--- JS/question.js
+++ JS/question.js
@@ -9,6 +9,7 @@ let level = findLevel();
 function findLevel() {
     if (endless.checked) {
         return 6;
+        //just push random number into computer array every turn
     } else if (leveled.checked) {
         return levelInput.value;
     }
